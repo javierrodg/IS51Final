3d7d98857471ec03702568be77fadba0aa7966be
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-05-13 15:58:34 -0700

    finshed structured english

---
+++ final.py
@@ -0,0 +1,13 @@
+"""
+Structured English
+
+This program takes in grades from a text file 
+It will display the number of grades
+It will display the average grade
+within a function that also displays the percentage of grades that
+are above the average grade shown.
+Grades above average will be done using a function calculating the 
+average, display the number of grades, and percent of grades above 
+average.
+
+"""
